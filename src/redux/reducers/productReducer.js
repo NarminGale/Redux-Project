54196b863bc2477e3c2f6a377b8eadf5a7544605
NarminGale <narmingale@gmail.com>
2022-01-27 15:14:51 +0400

    Created project structure

---
+++ src/redux/reducers/productReducer.js
@@ -0,0 +1,18 @@
+import {ActionTypes} from "../constants/action-types";
+
+const initialState = {
+    products: [{
+        id: 1,
+        title: 'Anna',
+        category: 'programmer'
+    }]
+}
+
+export const productReducer = (state, {type, payload}) => {
+    switch (type) {
+        case ActionTypes.SET_PRODUCTS:
+            return this.state
+        default:
+            return state
+    }
+}
