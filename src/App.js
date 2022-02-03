0b86086b8d43b8216597b81ed33c6325ec4b578e
NarminGale <narmingale@gmail.com>
2022-02-03 11:06:26 +0400

    chore: remove unnecessary codes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Header from './containers/Header'
 import ProductListing from "./containers/ProductListing";
-// import ProductComponent from "./containers/ProductComponent";
 import ProductDetails from "./containers/ProductDetail";
 
 function App() {
@@ -13,7 +12,6 @@ function App() {
         <Routes>
           <Route path="/" exact element={<ProductListing/>}/>
           <Route path="/product/:product" exact element={<ProductDetails/>}/>
-          {/*<Route path="/" exact component={ProductComponent}/>*/}
           <Route>404 NOT FOUND</Route>
         </Routes>
       </Router>
